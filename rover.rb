53343b5a693654f057e6f868d04d875c42fa9214
Benny <bennyxnguyen@gmail.com>
2016-06-05 17:27:39 -0400

    done rover controls

--- rover.rb
+++ rover.rb
@@ -1,29 +1,33 @@
 class Rover
 
-attr_accessor :position_x, :position_y, :current_direction
+attr_accessor :x_coordinate, :y_coordinate, :direction
 
-  def initialize
+  def initialize(x_coordinate, y_coordinate, direction)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @direction = direction
+  end
+
 
   def read_instruction(instructions)
-    instructions.each do |command|
-      if command == "L" || command == "R"
-      turn(command)
-      else command == "M"
+    instructions.each do |input|
+      if input == "L" || input == "R"
+      turn(input)
+    else input == "M"
         move
       end
     end
   end
 
 
-
   def turn(turn_direction)
-    if (direction = "N" && command == "L" || direction = "S" && command == "R")
+    if (direction = "N" && command == "L") || (direction = "S" && command == "R")
       @direction = "W"
-    elsif (direction = "E" && command == "L" || direction = "W" && command == "R")
+    elsif (direction = "E" && command == "L") || (direction = "W" && command == "R")
       @direction = "N"
-    elsif (direction = "W" && command == "L" || direction = "E" && command == "R")
+    elsif (direction = "W" && command == "L") || (direction = "E" && command == "R")
       @direction = "S"
-    elsif (direction = "S" && command == "L" || direction = "N" && command == "R")
+    elsif (direction = "S" && command == "L") || (direction = "N" && command == "R")
       @direction = "E"
     end
 
@@ -31,8 +35,21 @@ attr_accessor :position_x, :position_y, :current_direction
 
 
   def move
-
+    if direction  == "N"
+      @y_coordinate += 1
+    elsif direction == "S"
+      @y_coordinate -= 1
+    elsif direction == "E"
+      @x_coordinate += 1
+    elsif direction == "W"
+      @x_coordinate -= 1
+    end
   end
+
+
+end
+
+
 
 
   def status "The rover is currently #{position_x}, #{position_y}"
