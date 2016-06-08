1cd6366f8c6a05021cea667a69ee644b623f45f3
Benny <bennyxnguyen@gmail.com>
2016-06-07 20:38:20 -0400

    adding cat, paperboy, rover

--- mission_control.rb
+++ mission_control.rb
@@ -1,3 +1,9 @@
 require_relative 'rover'
 
-puts "what is the"
+puts "What is the plateau size?"
+plateau_size = gets.chomp
+
+puts "What is the initial position and header for the first rover?"
+
+
+puts "What are the series of moves for the first rover?"
